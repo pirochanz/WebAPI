e144f642fb16c02508f49af31b16cdaf84de8dc1
pirochanz <cat_sasami@yahoo.co.jp>
2021-07-22 18:58:36 +0900

    mvc edit

--- MvcApl/MvcApl/Controllers/DefaultController.cs
+++ MvcApl/MvcApl/Controllers/DefaultController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcApl.Extensions;
 
 namespace MvcApl.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        [HttpGet]
         public ActionResult Index()
         {
             ViewData["msg"] = "hello";
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [Button(ButtonName = "Search")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string search, string clear)
+        {
+            if (search != null)
+            {
+                Console.WriteLine("search");
+            }
+            if (clear != null)
+            {
+                Console.WriteLine("clear");
+            }
+
             return View();
         }
     }
